995c6786f2e0225ae8d717e82a192398201e776b
kmtvb <smv995@gmail.com>
2016-05-31 15:29:14 +0300

    cs fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,21 +10,18 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all(); 
+        $users = User::all();
         $logged = Auth::check();
         return view('user', ['users' => $users, 'logged' => $logged]);
     }
     
     public function deactivate($id)
     {
-        if (Auth::check())
-        {
-            if (Auth::user()->id == $id)
-            {
+        if (Auth::check()) {
+            if (Auth::user()->id == $id) {
                 return redirect()->route('user')->with('message', 'can\'t deactivate yourself!');
             }
-            if (User::find($id)->active == 0)
-            {
+            if (User::find($id)->active == 0) {
                 return redirect()->route('user')->with('message', 'user already deactivated!');
             }
             User::find($id)->update(['active' => 0]);
@@ -35,14 +32,11 @@ class UserController extends Controller
     
     public function activate($id)
     {
-        if (Auth::check())
-        {
-            if (Auth::user()->id == $id)
-            {
+        if (Auth::check()) {
+            if (Auth::user()->id == $id) {
                 return redirect()->route('user')->with('message', 'can\'t activate yourself!');
             }
-            if (User::find($id)->active == 1)
-            {
+            if (User::find($id)->active == 1) {
                 return redirect()->route('user')->with('message', 'user already activated!');
             }
             User::find($id)->update(['active' => 1]);
